dbc6575a0fd88462366212614db54fe1f1674d92
Demitra Del Fiacco <ddelfiacco99@gmail.com>
2019-05-21 22:52:16 -0500

    added custom timepicker

--- roomfinderApp/src/Pages/criteriaPage.js
+++ roomfinderApp/src/Pages/criteriaPage.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react'
-import { Segment, Header, Container, Grid, Form, Button } from 'semantic-ui-react'
+import { Segment, Header, Container, Grid, Form, Button, Input } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
-import { DateInput, TimeInput } from 'semantic-ui-calendar-react'
 import moment from 'moment'
-
+import { DateInput, TimeInput } from 'semantic-ui-calendar-react'
 
 const libraries = [
   { key: 'mudd', text: 'Mudd', value: 'mudd'},
   { key: 'main', text: 'Main', value: 'main'}
 ]
+
+const addTimes = (nextHr, ampm, selectTimes) => {
+  if (ampm === ' AM') {
+    for (let i = nextHr; i <= 11; i++) {
+      selectTimes.push(i.toString() + ':00' + ' AM')
+      selectTimes.push(i + ':30' + ' AM')
+    } 
+    selectTimes.push('12:00 PM')
+    selectTimes.push('12:30 PM')
+    for (let j = 1; j <= 11; j++) {
+      selectTimes.push(j + ':00' + ' PM')
+      selectTimes.push(j + ':30' + ' PM')
+    }
+  } else {
+    for (let i = nextHr; i <= 11; i++) {
+      selectTimes.push(i + ':00' + ampm)
+      selectTimes.push(i + ':30' + ampm)
+    }
+  }
+}
 
 class CriteriaPage extends Component {
   constructor(props) {
@@ -37,6 +56,61 @@ class CriteriaPage extends Component {
   }
 
   render () {
+    const timeFromOptions = () => {
+      let currTime = moment().format('LT')
+      let n = currTime.indexOf(':') + 1
+      let currHour = parseInt(currTime.substring(0,n))
+      let nextHour = (currHour === 12) ? 1 : currHour + 1
+      let currMin = parseInt(currTime.substring(n)) 
+      var selectTimes= []
+
+      if (this.state.date === '') {
+        selectTimes = []
+      } 
+      else if (this.state.date === moment().format('MM-DD-YYYY'))  {
+        if (currTime.includes('PM')) {
+          if (currMin < 30) 
+            selectTimes.push(currHour + ":30 PM")
+          addTimes(nextHour, ' PM', selectTimes)
+        }
+        else if (currTime.includes('AM')) {
+          if (currMin < 30) 
+            selectTimes.push(currHour + ":30 AM")
+          addTimes(nextHour, ' AM', selectTimes)
+        } 
+      } 
+      else {
+        selectTimes.push('12:00 AM')
+        selectTimes.push('12:30 AM')
+        addTimes(1, ' AM', selectTimes)
+      }
+    
+      let startTimeObjects = []
+      for (let i = 0; i < selectTimes.length; i++) {
+        startTimeObjects.push(
+          { key: i.toString(), text: selectTimes[i], value: selectTimes[i]}
+        )
+      }
+      return startTimeObjects
+    }
+    const fromTimes = timeFromOptions() 
+
+    const timeToOptions = () => {
+      const val = this.state.timeFrom
+      if (this.state.timeFrom !== '') {
+        let index = fromTimes.map((obj) => { return obj.text; }).indexOf(val);
+        let start = parseInt(fromTimes[index].key) + 1
+        let end = start + 6
+        const selectTimes = []
+        for (let i = start; i < end; i++) {
+          selectTimes.push(fromTimes[i])
+        }
+        return selectTimes
+      } else return null 
+    }
+    const toTimes = timeToOptions()
+    console.log(toTimes)
+
     return ([
       <Segment textAlign='center' style={{borderRadius: '0px'}}>
         <Header 
@@ -65,41 +139,32 @@ class CriteriaPage extends Component {
                 onChange={this.handleChange}
               />
               <DateInput
+                pickerWidth="12px;"
                 label="Date"
                 name="date"
                 placeholder="MM/DD/YYYY"
                 value={this.state.date}
                 onChange={this.handleChange}
-                popupPosition="bottom center"
-                duration="0"
-                hideMobileKeyboard
-                closable
                 dateFormat="MM-DD-YYYY"
+                durati  on={0}
+                closable
                 minDate={moment().toDate()}
+                hideMobileKeyboard
+                popupPosition="bottom center"
               />
-              <TimeInput 
-                label="From:"
+              <Form.Select 
+                label="From: "
                 name="timeFrom"
-                placeholder="00:00"
+                options={fromTimes}
                 value={this.state.timeFrom}
                 onChange={this.handleChange}
-                duration="0"
-                closable
-                popupPosition="bottom center"
-                timeFormat="AMPM"
-                hideMobileKeyboard
               />
-              <TimeInput 
-                label="To:"
+              <Form.Select 
+                label="To: "
                 name="timeTo"
-                placeholder="00:00"
+                options={toTimes}
                 value={this.state.timeTo}
                 onChange={this.handleChange}
-                duration="0"
-                closable
-                popupPosition="bottom center"
-                timeFormat="AMPM"
-                hideMobileKeyboard
               />
               <Link to="/rooms">
                 <Button 
